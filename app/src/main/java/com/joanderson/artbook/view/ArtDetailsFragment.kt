4003a5b5e61d58f537e7b7f1fe0d7548322d24c0
Joanderson <joandersongsantos@yahoo.com>
2021-07-05 19:22:52 -0300

    feat: add view binding

--- app/src/main/java/com/joanderson/artbook/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/joanderson/artbook/view/ArtDetailsFragment.kt
@@ -2,25 +2,36 @@ package com.joanderson.artbook.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
 import com.joanderson.artbook.R
 import com.joanderson.artbook.databinding.FragmentArtDetailsBinding
+import com.joanderson.artbook.util.Status
+import com.joanderson.artbook.viewmodel.ArtViewModel
 import javax.inject.Inject
 
 class ArtDetailsFragment @Inject constructor(
     val glide : RequestManager
 ) : Fragment(R.layout.fragment_art_details){
 
+    lateinit var viewModel : ArtViewModel
+
     private var fragmentBinding : FragmentArtDetailsBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+
         val binding = FragmentArtDetailsBinding.bind(view)
         fragmentBinding = binding
+
+        subscribeToObservers()
+
         binding.ivArt.setOnClickListener {
             findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
         }
@@ -32,6 +43,38 @@ class ArtDetailsFragment @Inject constructor(
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+
+        binding.btSaveNewArt.setOnClickListener {
+            viewModel.makeArt(
+                binding.tvArtName.text.toString(),
+                binding.tvArtistName.text.toString(),
+                binding.TvArtYear.text.toString()
+            )
+        }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.selectedImageUrl.observe(viewLifecycleOwner) { url ->
+            fragmentBinding?.let {
+                glide.load(url).into(it.ivArt)
+            }
+        }
+
+        viewModel.insertArtMessage.observe(viewLifecycleOwner) {
+            when(it.status) {
+                Status.SUCCESS -> {
+                    Toast.makeText(requireContext(), "Added successfully", Toast.LENGTH_LONG).show()
+                    findNavController().popBackStack()
+                    viewModel.resetInsertArtMsg()
+                }
+                Status.ERROR -> {
+                    Toast.makeText(requireContext(), it.message?: "Unknown Error", Toast.LENGTH_LONG).show()
+                }
+                Status.LOADING -> {
+
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/joanderson/artbook/view/ImageApiFragment.kt
+++ app/src/main/java/com/joanderson/artbook/view/ImageApiFragment.kt
@@ -2,16 +2,82 @@ package com.joanderson.artbook.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.joanderson.artbook.R
 import com.joanderson.artbook.adapter.ImageRecyclerAdapter
+import com.joanderson.artbook.databinding.FragmentImageApiBinding
+import com.joanderson.artbook.util.Status
+import com.joanderson.artbook.viewmodel.ArtViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ImageApiFragment @Inject constructor(
     private val imageRecyclerAdapter: ImageRecyclerAdapter
 ) : Fragment(R.layout.fragment_image_api) {
 
+    lateinit var viewModel : ArtViewModel
+
+    private var fragmentBinding : FragmentImageApiBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+
+        val binding = FragmentImageApiBinding.bind(view)
+        fragmentBinding = binding
+
+        var job : Job? = null
+
+        binding.etSearch.addTextChangedListener {
+            job?.cancel()
+            job = lifecycleScope.launch {
+                delay(1000)
+                it?.let {
+                    if (it.toString().isNotEmpty()) {
+                        viewModel.searchForImage(it.toString())
+                    }
+                }
+            }
+        }
+
+        subscribeToObservers()
+
+        binding.rvSearchResult.adapter = imageRecyclerAdapter
+        binding.rvSearchResult.layoutManager = GridLayoutManager(requireContext(), 3)
+        imageRecyclerAdapter.setOnItemClickListener {
+            findNavController().popBackStack()
+            viewModel.setSelectedImage(it)
+        }
+
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.imageList.observe(viewLifecycleOwner) {
+            when(it.status) {
+                Status.SUCCESS -> {
+                    val urls = it.data?.hits?.map { imageResult ->
+                        imageResult.previewURL
+                    }
+                    imageRecyclerAdapter.images = urls ?: listOf()
+                    fragmentBinding?.pbImageLoadProgress?.visibility = View.GONE
+                }
+                Status.ERROR -> {
+                    Toast.makeText(requireContext(), it.message ?: "Unknown Error", Toast.LENGTH_LONG).show()
+                    fragmentBinding?.pbImageLoadProgress?.visibility = View.GONE
+                }
+                Status.LOADING -> {
+                    fragmentBinding?.pbImageLoadProgress?.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
